1f8cdbc3e50cb3df0ea8fcc44c78abc4e9e57f7c
Samuel Mejía <150869146+Biggles-insano@users.noreply.github.com>
2024-04-10 13:27:08 -0600

    Create ColaPrioridad.java

---
+++ ColaPrioridad.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColaPrioridad {
+    private List<Proceso> heap;
+
+    public ColaPrioridad() {
+        heap = new ArrayList<>();
+    }
+
+    public void agregarProceso(Proceso proceso) {
+        heap.add(proceso);
+        int indiceActual = heap.size() - 1;
+        while (indiceActual > 0) {
+            int indicePadre = (indiceActual - 1) / 2;
+            if (heap.get(indiceActual).compareTo(heap.get(indicePadre)) < 0) {
+                Proceso temp = heap.get(indiceActual);
+                heap.set(indiceActual, heap.get(indicePadre));
+                heap.set(indicePadre, temp);
+                indiceActual = indicePadre;
+            } else {
+                break;
+            }
+        }
+    }
+
+    public Proceso obtenerProcesoSiguiente() {
+        if (heap.isEmpty()) {
+            return null;
+        }
+        Proceso proceso = heap.get(0);
+        heap.set(0, heap.get(heap.size() - 1));
+        heap.remove(heap.size() - 1);
+        int indiceActual = 0;
+        while (true) {
+            int hijoIzquierdo = 2 * indiceActual + 1;
+            int hijoDerecho = 2 * indiceActual + 2;
+            if (hijoIzquierdo >= heap.size()) {
+                break;
+            }
+            int indiceMinimo = hijoIzquierdo;
+            if (hijoDerecho < heap.size() && heap.get(hijoDerecho).compareTo(heap.get(hijoIzquierdo)) < 0) {
+                indiceMinimo = hijoDerecho;
+            }
+            if (heap.get(indiceActual).compareTo(heap.get(indiceMinimo)) > 0) {
+                Proceso temp = heap.get(indiceActual);
+                heap.set(indiceActual, heap.get(indiceMinimo));
+                heap.set(indiceMinimo, temp);
+                indiceActual = indiceMinimo;
+            } else {
+                break;
+            }
+        }
+        return proceso;
+    }
+
+    public boolean hayProcesos() {
+        return !heap.isEmpty();
+    }
+}
